7bd1331a59f7839d36c47ee48afe540b8a4746e6
TL <TL@gmail.com>
2020-04-10 15:46:28 +0530

    Dev2 adding logic in client appli

--- DemoProj1/src/com/nt/test/Test1.java
+++ DemoProj1/src/com/nt/test/Test1.java
@@ -1,5 +1,18 @@
 package com.nt.test;
 
-public class Test1 {
+import java.util.Date;
 
+import com.nt.beans.Utility;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		Utility util=null;
+		util=new Utility();
+		Date d=util.getDate();
+		System.out.println(d);
+		util.m1();
+		
+	}
+	
 }
