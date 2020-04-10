807159885643c38274c83823e8fec8a1bf2794fa
TL <TL@gmail.com>
2020-04-10 15:31:55 +0530

    dev2 adding more methods to utility class

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -1,11 +1,14 @@
 package com.nt.beans;
-
 import java.util.Date;
 
 public class Utility {
 	
 	public Date getDate() {
 		return new Date();
+	}
+	
+	public void m1() {
+		System.out.println("m1");
 	}
 	
 }
